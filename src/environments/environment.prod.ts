5472917ef06033083b55565191bfdf1dd1cb3803
edwin95florez <edwin.95.florez@gmail.com>
2020-05-09 14:51:39 -0500

    Aplicaciones varias Angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apis: {
-    cocktail: 'https://www.thecocktaildb.com/api/json/v1/1/'
+    cocktail: 'https://www.thecocktaildb.com/api/json/v1/1/',
+    country: 'https://restcountries.eu/rest/v2/regionalbloc/'
   }
 };
